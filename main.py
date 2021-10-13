0bd3cccf9a19351b9e29cf3e5b354fabfb2adb58
apelissier <alexispelissier2002@gmail.com>
2021-10-13 16:07:15 +0200

    Find Actors

--- main.py
+++ main.py
@@ -92,9 +92,37 @@ def actors():
     return page_return('SUCCESS', 200, 'Actors')
 
 
-@app.route("/actor/find")
+@app.route("/actors/find")
 def find_actors():
-    return page_return('SUCCESS', 200, 'Search Actors')
+    collections = db['actors']
+    search = {}
+    actors = []
+
+    actor_args = get_args(['name', 'age', 'genre'])
+    if actor_args[0] is None and actor_args[1] is None and actor_args[2] is None:
+        return page_return('ERROR', 400, 'Aucun paramètre de recherche')
+
+    if actor_args[0]:
+        if not actor_args[0].isalnum() or actor_args[0].isdigit():
+            return page_return('ERROR', 400, 'Erreur dans le paramètre NAME')
+        search['name'] = {'$regex': actor_args[0]}
+
+    if actor_args[1]:
+        if not actor_args[1].isalnum():
+            return page_return('ERROR', 400, 'Erreur dans le paramètre AGE')
+        search['age'] = int(actor_args[1])
+
+    if actor_args[2]:
+        search['genre'] = actor_args[2]
+    if not len(search) == 0:
+        for actor in collections.find(search):
+            actors.append(actor)
+    else:
+        return page_return('ERROR', 200, 'Verifiez paramètres')
+    if len(actors) == 0:
+        return page_return('SUCCESS', 200, 'Aucun acteur')
+
+    return page_return('SUCCESS', 200, str(actors))
 
 
 if __name__ == '__main__':
