31af7704c2d75798c502299143f5e4e0340a3739
apelissier <alexispelissier2002@gmail.com>
2021-10-13 11:49:56 +0200

    ajout 1er film

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask import request
 from flask import make_response
-from pymongo import mongo_client
+from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
 host = os.environ["HOST"]
 
+username = os.environ["DB_USER"]
+password = os.environ["DB_PASS"]
+cluster = os.environ["CLUSTER"]
+database_name = os.environ["DB"]
+collection_name = "movies"
 
-# username = os.environ["DB_USER"]
-# password = os.environ["DB_PASS"]
-# cluster = os.environ["CLUSTER"]
-# database_name = os.environ["DB"]
-# collection_name = os.environ["COLLECTION"]
+client = MongoClient(f"mongodb+srv://{username}:{password}@{cluster}/{database_name}?retryWrites=true&w=majority")
+db = client[database_name]
+collection = db[collection_name]
 
 
 def page_return(type, code, message):
@@ -21,12 +24,17 @@ def page_return(type, code, message):
 
 @app.route("/")
 def home():
+    collection.insert_one(
+        {'_id': 0, 'title': 'Deadpool', 'category': 'Super-Hero', 'synopsis': "Deadpool, est l'anti-héros le plus atypique de l'univers Marvel. A l'origine, il s'appelle Wade Wilson : un ancien militaire des Forces Spéciales devenu mercenaire. Après avoir subi une expérimentation hors norme qui va accélérer ses pouvoirs de guérison, il va devenir Deadpool. Armé de ses nouvelles capacités et d'un humour noir survolté, Deadpool va traquer l'homme qui a bien failli anéantir sa vie.", 'distribution': ['Ryan Reynolds',
+                                                                                                   'Morena Baccarin'],
+         'release_date': '2016', 'duration': '108','likes': 1028, 'dislikes': 256})
+
     return page_return('SUCCESS', 200, 'Accueil')
 
 
-@app.route("/movies")
+@app.route("/movies", methods=["GET"])
 def movies():
-    return page_return('SUCCESS', 200, 'Movies')
+    return page_return('SUCCESS', 200, 'Get_Movies')
 
 
 @app.route("/movies/find")
@@ -34,8 +42,8 @@ def find_movies():
     return page_return('SUCCESS', 200, 'Search Movies')
 
 
-@app.route("/vote/id")
-def vote():
+@app.route("/vote/<int:id>")
+def vote(id):
     return page_return('SUCCESS', 200, 'Vote')
 
 
