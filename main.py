6314c8d93b99adab5de8c53063ffe2ff71225f09
apelissier <alexispelissier2002@gmail.com>
2021-10-13 13:48:32 +0200

    add movies list

--- main.py
+++ main.py
@@ -11,11 +11,8 @@ username = os.environ["DB_USER"]
 password = os.environ["DB_PASS"]
 cluster = os.environ["CLUSTER"]
 database_name = os.environ["DB"]
-collection_name = "movies"
-
 client = MongoClient(f"mongodb+srv://{username}:{password}@{cluster}/{database_name}?retryWrites=true&w=majority")
 db = client[database_name]
-collection = db[collection_name]
 
 
 def page_return(type, code, message):
@@ -24,17 +21,19 @@ def page_return(type, code, message):
 
 @app.route("/")
 def home():
-    collection.insert_one(
-        {'_id': 0, 'title': 'Deadpool', 'category': 'Super-Hero', 'synopsis': "Deadpool, est l'anti-héros le plus atypique de l'univers Marvel. A l'origine, il s'appelle Wade Wilson : un ancien militaire des Forces Spéciales devenu mercenaire. Après avoir subi une expérimentation hors norme qui va accélérer ses pouvoirs de guérison, il va devenir Deadpool. Armé de ses nouvelles capacités et d'un humour noir survolté, Deadpool va traquer l'homme qui a bien failli anéantir sa vie.", 'distribution': ['Ryan Reynolds',
-                                                                                                   'Morena Baccarin'],
-         'release_date': '2016', 'duration': '108','likes': 1028, 'dislikes': 256})
-
     return page_return('SUCCESS', 200, 'Accueil')
 
 
 @app.route("/movies", methods=["GET"])
 def movies():
-    return page_return('SUCCESS', 200, 'Get_Movies')
+    collection = db["movies"]
+    movie_list = []
+    for movie in collection.find():
+        movie_list.append(movie)
+    if len(movie_list) == 0:
+        return page_return('SUCCESS', 200, 'No Movies')
+    else:
+        return page_return('SUCCESS', 200, str(movie_list))
 
 @app.route("/movies", methods=["POST"])
 def add_movies():
