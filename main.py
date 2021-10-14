c852e6b8c4fc7e99a91564a11c9097783e50d89e
Nohan75 <nohanmarielouise@gmail.com>
2021-10-14 10:13:28 +0200

    function for search a movie

--- main.py
+++ main.py
@@ -92,9 +92,43 @@ def delete_movie():
         return page_return('ERROR', 404, 'Aucun film à cet Id')
 
 
-@app.route("/movies/find")
+@app.route("/movies/find", methods=["GET"])
 def find_movies():
-    return page_return('SUCCESS', 200, 'Search Movies')
+    """
+    Cette fonction permet de trouvé un film par rapport à ces params
+    :return: Un message d'erreur ou de succès avec le film recherché
+    """
+    collections = db['movies']
+    search = {}
+    movies = []
+
+    movie_args = get_args(['title', 'category', 'release_date'])
+    if movie_args[0] is None and movie_args[1] is None and movie_args[2] is None:
+        return page_return('ERROR', 400, 'Aucun paramètre de recherche')
+
+    if movie_args[0]:
+        if movie_args[0].isdigit():
+            return page_return('ERROR', 400, 'Erreur dans le paramètre TITLE')
+        search['title'] = {'$regex': movie_args[0]}
+
+    if movie_args[1]:
+        if not movie_args[1].isalnum() or movie_args[1].isdigit():
+            return page_return('ERROR', 400, 'Erreur dans le paramètre CATEGORY')
+        search['category'] = {'$regex': movie_args[1]}
+
+    if movie_args[2]:
+        if not movie_args[2].isalnum() or not movie_args[2].isdigit():
+            return page_return('ERROR', 400, 'Erreur dans le paramètre RELEASE_DATE')
+        search['release_date'] = movie_args[2]
+    if not len(search) == 0:
+        for movie in collections.find(search):
+            movies.append(movie)
+    else:
+        return page_return('ERROR', 200, 'Verifiez paramètres')
+    if len(movies) == 0:
+        return page_return('SUCCESS', 200, 'Aucun acteur')
+
+    return page_return('SUCCESS', 200, str(movies))
 
 
 @app.route("/vote/<int:id>", methods=["POST"])
